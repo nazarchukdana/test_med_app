4217b42c908c961e2db1ae997fd2ff0ce9b948a9
nazarchukdana <nazarchuk.dana05@gmail.com>
2024-10-12 17:07:48 -0400

    connecting mongo

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:QM30TTNy66DWrYptTuAByNvi@172.21.196.155:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
