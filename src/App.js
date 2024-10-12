0a667ea149ee1954a7bcf1d5a389a7fd2a2abd0a
nazarchukdana <nazarchuk.dana05@gmail.com>
2024-10-12 17:55:50 -0400

    adding landing page component, navbar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+import Landing_page from './Components/Landing_Page/Landing_Page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+          <Landing_page/>
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
